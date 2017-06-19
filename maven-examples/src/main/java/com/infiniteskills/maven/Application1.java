b272bdf5acd6c461bf13b39a8f30e835cbd28bcb
CrisAce <CristAce@github.com>
2017-06-19 10:34:18 -0400

    first change, add a simple output change

---
+++ maven-examples/src/main/java/com/infiniteskills/maven/Application1.java
@@ -0,0 +1,13 @@
+package com.infiniteskills.maven;
+
+public class Application1 {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Learning Git");
+		
+		System.out.println("test 1");
+		
+	}
+
+}
---
+++ maven-examples/src/nonstandard/java/com/infiniteskills/maven/Application2.java
@@ -0,0 +1,10 @@
+package com.infiniteskills.maven;
+
+public class Application2 {
+
+	public static void main(String[] args) {
+System.out.println("Hello World 2!");
+		
+	}
+
+}
